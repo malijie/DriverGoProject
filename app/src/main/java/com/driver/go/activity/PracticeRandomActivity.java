84d980e55a8e7a6be98767714242e41f6d242d56
malijie <190223629@qq.com>
2017-02-22 12:25:00 +0800

    commit ui

---
+++ app/src/main/java/com/driver/go/activity/PracticeRandomActivity.java
@@ -0,0 +1,24 @@
+package com.driver.go.activity;
+
+import android.os.Bundle;
+
+/**
+ * Created by Administrator on 2016/12/1.
+ */
+public class PracticeRandomActivity extends DriverBaseActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.random_practise);
+    }
+
+    @Override
+    public void initView() {
+
+    }
+
+    @Override
+    public void initData() {
+
+    }
+}
---
+++ app/src/main/java/com/driver/go/activity/ReciteSubjectActivity.java
@@ -0,0 +1,24 @@
+package com.driver.go.activity;
+
+import android.os.Bundle;
+
+/**
+ * Created by Administrator on 2016/12/1.
+ */
+public class ReciteSubjectActivity extends DriverBaseActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.random_practise);
+    }
+
+    @Override
+    public void initView() {
+
+    }
+
+    @Override
+    public void initData() {
+
+    }
+}
