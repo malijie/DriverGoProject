33112dbc096d074ef20b71e1fdcebb532499186f
malijie <190223629@qq.com>
2017-03-21 22:15:44 +0800

    commit welcome

---
+++ app/src/main/java/com/driver/go/activity/WelcomeActivity.java
@@ -0,0 +1,59 @@
+package com.driver.go.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.widget.ImageView;
+
+import com.driver.go.activity.base.DriverBaseActivity;
+import com.driver.go.R;
+import com.driver.go.utils.SharePreferenceUtil;
+
+/**
+ * Created by Administrator on 2017/3/21.
+ */
+
+public class WelcomeActivity extends DriverBaseActivity{
+    private ImageView mImageWelcome = null;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);//ȫ����ʾ
+        super.setContentView(R.layout.welcome_layout);
+
+        mImageWelcome = (ImageView) findViewById(R.id.id_welcome_image);
+        AlphaAnimation alpha = new AlphaAnimation(0.1f, 1.0f);
+        alpha.setDuration(3000);
+        mImageWelcome.setAnimation(alpha);
+        alpha.setAnimationListener(new Animation.AnimationListener() {
+
+            @Override
+            public void onAnimationStart(Animation animation) {
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                if(SharePreferenceUtil.loadIsFirstLoaded()){
+                    Intent itMain = new Intent(WelcomeActivity.this,GuideActivity.class);
+                    startActivity(itMain);
+                    WelcomeActivity.this.finish();
+                    SharePreferenceUtil.saveIsFirstLoaded(false);
+
+                }else{
+                    Intent itMain = new Intent(WelcomeActivity.this,MainActivity.class);
+                    startActivity(itMain);
+                    WelcomeActivity.this.finish();
+                }
+            }
+        });
+    }
+}
--- app/src/main/java/com/driver/go/utils/SharePreferenceUtil.java
+++ app/src/main/java/com/driver/go/utils/SharePreferenceUtil.java
@@ -9,10 +9,21 @@ import com.driver.go.base.DriverGoApplication;
  */
 public class SharePreferenceUtil {
     public static final String SP_USER_INFO = "user_info";
+    public static final String FIRST_LOADED_KEY = "first_loaded";
     public static final String SUBJECT1_ORDER_QUESTION_CURRENT_INDEX_KEY = "subject1_order_question_index";
     public static final String SUBJECT1_RECITE_QUESTION_CURRENT_INDEX_KEY = "subject1_recite_question_index";
     public static final String SUBJECT4_ORDER_QUESTION_CURRENT_INDEX_KEY = "subject4_order_question_index";
     public static final String SUBJECT4_RECITE_QUESTION_CURRENT_INDEX_KEY = "subject4_recite_question_index";
+
+    public static void saveIsFirstLoaded(boolean isFirstLoaded){
+        DriverGoApplication.sContext.getSharedPreferences(SP_USER_INFO, Context.MODE_PRIVATE).edit()
+                .putBoolean(FIRST_LOADED_KEY,isFirstLoaded).commit();
+    }
+
+    public static boolean loadIsFirstLoaded(){
+        return DriverGoApplication.sContext.getSharedPreferences(SP_USER_INFO, Context.MODE_PRIVATE)
+                .getBoolean(FIRST_LOADED_KEY,true);
+    }
 
     public static void saveSubject1OrderQuestionIndex(int index){
             DriverGoApplication.sContext.getSharedPreferences(SP_USER_INFO, Context.MODE_PRIVATE).edit().putInt(SUBJECT1_ORDER_QUESTION_CURRENT_INDEX_KEY,index).commit();
