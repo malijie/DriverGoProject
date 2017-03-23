8e28c2e043a179974411a774013714d2e9218f5d
malijie <190223629@qq.com>
2017-03-23 16:34:41 +0800

    update icons

---
+++ app/src/main/java/com/driver/go/wap/WapManager.java
@@ -0,0 +1,38 @@
+package com.driver.go.wap;
+
+import android.content.Context;
+
+import cn.waps.AppConnect;
+
+/**
+ * Created by malijie on 2017/3/23.
+ */
+
+public class WapManager {
+    public static WapManager sWapManager = null;
+    private Context mContext = null;
+    private AppConnect mAppConnect = null;
+
+    private WapManager(Context context){
+        mContext = context;
+        mAppConnect.getInstance(WapProfile.WAP_APP_ID,WapProfile.WAP_APP_PID,context);
+    }
+
+    public static WapManager getInstance(Context context){
+        if(sWapManager == null){
+            synchronized (WapManager.class){
+                if(sWapManager == null){
+                    sWapManager = new WapManager(context);
+                }
+            }
+        }
+        return sWapManager;
+    }
+
+
+    public void close(){
+        mAppConnect.close();
+    }
+
+
+}
---
+++ app/src/main/java/com/driver/go/wap/WapProfile.java
@@ -0,0 +1,11 @@
+package com.driver.go.wap;
+
+/**
+ * Created by malijie on 2017/3/23.
+ */
+
+public class WapProfile {
+    public static final String WAP_APP_ID = "1360f3c9e42796b0e81aec2d92cf7f95";
+    public static final String WAP_APP_PID = "qq";
+
+}
