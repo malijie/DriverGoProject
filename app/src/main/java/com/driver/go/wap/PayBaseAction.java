8f8109c41a7acc1f1423022bfa1c2b6d0b29cfa3
malijie <190223629@qq.com>
2017-04-11 20:36:43 +0800

    update app name

--- app/src/main/java/com/driver/go/wap/PayBaseAction.java
+++ app/src/main/java/com/driver/go/wap/PayBaseAction.java
@@ -12,15 +12,16 @@ import com.wanpu.pay.PayConnect;
 
 public class PayBaseAction {
     protected PayConnect mPayConnect = null;
-    public static final String GOODS_NAME_EXAM = "驾照一点通VIP会员";
+    public static final String GOODS_NAME_EXAM = "驾考万能通VIP会员";
     public static final String GOODS_DESCR_EXAM = "购买VIP会员，享受真题模拟考试服务";
     public static final float PRICE_EXAM = 4.99F;
 
-    public static final String GOODS_NAME_VIP = "驾照一点通VIP会员";
+    public static final String GOODS_NAME_VIP = "驾考万能通VIP会员";
     public static final String GOODS_DESCR_VIP = "购买VIP会员，享受查看真题解释服务";
     public static final float PRICE_VIP = 4.99F;
 
     public PayBaseAction(Activity activity){
-        mPayConnect = PayConnect.getInstance(WapProfile.WAP_APP_ID,WapProfile.WAP_APP_PID,activity);
+//        mPayConnect = PayConnect.getInstance(WapProfile.WAP_APP_ID,WapProfile.WAP_APP_PID,activity);
+        mPayConnect = PayConnect.getInstance(activity);
     }
 }
